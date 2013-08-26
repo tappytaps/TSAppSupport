8cf5adcf3db58a6a80995d22c25ca77de0f13d5e
Jindrich Sarson <jindra@tappytaps.com>
2013-08-26 11:27:53 +0200

    Initial Commit

---
+++ TSAppSupport/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TSAppSupport
+//
+//  Created by JS on 8/26/13.
+//  Copyright (c) 2013 TappyTaps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TSAppSupport/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TSAppSupport
+//
+//  Created by JS on 8/26/13.
+//  Copyright (c) 2013 TappyTaps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
